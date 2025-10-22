9dba062023a99b4002c0f1338d0056d717f4bcc7
Maxim Nikolsky <maxim.nikolskiy@fors.ru>
2025-10-22 11:36:05 +0300

    Задание 2. Забменил БД телеметрии на ClickHouse

--- telemetry-db/init.sql
+++ telemetry-db/init.sql
@@ -1,11 +1,14 @@
 create table if not exists telemetry(
-    id serial primary key,
-    user_email varchar,
-    device_id uuid,
-    metric_value varchar,
-    metric_unit varchar,
-    metric_timestamp timestamp
-);
+    id UUID default generateUUIDv4(),
+    user_email String,
+    device_id UUID,
+    metric_value String,
+    metric_unit String,
+    metric_timestamp DateTime
+)
+    ENGINE = MergeTree()
+    ORDER BY  (user_email, device_id, metric_timestamp);
 
-copy telemetry(user_email, device_id, metric_value, metric_unit, metric_timestamp)
-    from '/docker-entrypoint-initdb.d/telemetry.csv' delimiter ',' csv header;
+insert into telemetry(user_email, device_id, metric_value, metric_unit, metric_timestamp)
+    from infile '/docker-entrypoint-initdb.d/telemetry.csv'
+    format CSVWithNames;
